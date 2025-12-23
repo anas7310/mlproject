55275c77772013272bf6ebfa02a9ef0d0479260f
Anas Yar Khan <yarkhananas25@gmail.com>
2025-12-23 08:49:52 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+##Setup.py is used to build application as a package itself
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(filename:str)->List[str]:
+    """
+        This function will return the list of requirements
+    """
+
+    with open(filename) as f:
+        requirements=f.readlines()
+        requirements =[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='AnasKhan',
+    author_email='yarkhananas42@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
+
+
