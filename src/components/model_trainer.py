efdab13951737392ecec051e48887ed2805366f1
Anas Yar Khan <yarkhananas25@gmail.com>
2025-12-28 11:20:09 +0530

    Hyperparameter Tuning and Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -13,10 +13,12 @@ from sklearn.svm import SVR
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 
 from sklearn.model_selection import RandomizedSearchCV
 from catboost import CatBoostRegressor
 from xgboost import XGBRegressor
+from sklearn.base import BaseEstimator
 
 from src.exception import CustomException
 from src.utils import save_object
@@ -25,6 +27,10 @@ from src.utils import evaluate_models
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path = os.path.join('artifacts', 'model.pkl')
+
+class CompatibleCatBoostRegressor(CatBoostRegressor, BaseEstimator):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
 class ModelTrainer:
     def __init__(self):
@@ -50,10 +56,70 @@ class ModelTrainer:
                 "Random Forest": RandomForestRegressor(),
                 "AdaBoost": AdaBoostRegressor(),
                 "XGBoost": XGBRegressor(),
-                "CatBoost": CatBoostRegressor()
+                "CatBoost": CompatibleCatBoostRegressor(verbose=False, allow_writing_files=False),
+                "GradientBoosting":GradientBoostingRegressor()
             }
 
-            model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            params = {
+                "Linear Regression": {},
+                "Ridge": {
+                    'alpha': [0.1, 1.0, 10.0, 100.0],
+                    'solver': ['auto', 'svd', 'cholesky', 'lsqr']
+                },
+                "Lasso": {
+                    'alpha': [0.001, 0.01, 0.1, 1.0, 10.0],
+                    'selection': ['cyclic', 'random']
+                },
+                "SVR": {
+                    'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+                    'C': [0.1, 1, 10, 100],
+                    'epsilon': [0.1, 0.2, 0.5]
+                },
+                "KNN": {
+                    'n_neighbors': [3, 5, 7, 9, 11],
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+                },
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter': ['best', 'random'],
+                    'max_depth': [None, 5, 10, 20],
+                    'min_samples_split': [2, 5, 10]
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'max_features': ['sqrt', 'log2', None],
+                    'max_depth': [None, 5, 10]
+                },
+                "AdaBoost": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                    'learning_rate': [0.1, 0.01, 0.5, 0.001],
+                    'loss': ['linear', 'square', 'exponential']
+                },
+                "XGBoost": {
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                    'max_depth': [3, 5, 7],
+                    'subsample': [0.6, 0.8, 1.0]
+                },
+                "CatBoost": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100],
+                    'verbose': [False]  # Keeps the output clean during tuning
+                },
+                "GradientBoosting": {
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
+            }
+
+            model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, params=params)
 
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[
--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,9 @@ import sys
 import dill
 import numpy as np
 import pandas as pd
+from src.logger import logging
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -19,12 +21,23 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train, X_test, y_train, y_test, models):
+def evaluate_models(X_train, X_test, y_train, y_test, models, params):
     try:
         report={}
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            logging.info("GridSearchCV HyperParameter Tuning done successfully.")
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+
+            # model.fit(X_train, y_train)
+
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
